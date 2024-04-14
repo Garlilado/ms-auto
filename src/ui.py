6a3197ee911ad6ad974a7834440069ef1150bf89
“JJ” <164868975@qq.com>
2024-04-14 12:03:02 +0800

    feat: first UI with device status check and map choosen

---
+++ src/ui.py
@@ -0,0 +1,57 @@
+import tkinter as tk
+from tkinter import ttk
+from utils import check_connection, add_to_accountlist
+from globals import DEVICES_NAMES
+from player import *
+from ui_globals import *
+
+
+root.grid_rowconfigure(0, weight=0)  # Allow the first row to expand when the window size changes
+root.grid_rowconfigure(1, weight=1)  # Allow the second row to expand more when the window size changes
+root.grid_columnconfigure(0, weight=1)  # Allow the column to expand when the window size changes
+
+# ---------------------------------Arrange the status Notebook----------------------------------
+status_frm.grid(sticky='nsew')  # Make the frame fill the window
+status_notebook.add(status_frm, text='主号组连接')
+status_notebook.grid(column=0, row=0, sticky='nsew')  # Add the first Notebook to the grid
+status_notebook_title.grid(column=0, row=0)  # Place the title at the top of the grid
+refresh_button.grid(column=1, row=0)  # Place the refresh button at the top of the grid
+acc_choose_header.grid(column=0, row=1, sticky='nsew')  # Place the title at the top of the grid
+acc_name_header.grid(column=1, row=1, sticky='nsew')  # Place the title at the top of the grid
+dev_status_header.grid(column=2, row=1, sticky='nsew')  # Place the title at the top of the grid
+
+connection_status = check_connection(status_frm, accountlist=accountlist, holder_combobox=holder_combobox)  # Call the check function to initialize the connection status
+
+for i in range(4):
+    var = tk.BooleanVar()
+    var.set(connection_status[i])  # Set the initial value to True
+    vars.append(var)
+    state = 'normal' if connection_status[i] else 'disabled'  # Set the state based on the connection status
+    chk = tk.Checkbutton(status_frm, text='', variable=var, command=lambda account=keys[i], var=var, accountlist=accountlist, holder_combobox=holder_combobox: add_to_accountlist(account, var, accountlist, holder_combobox), state=state)
+    chk.grid(column=0, row=i+2, sticky='nsew')  # Make the checkbox fill the cell    
+    checkbuttons.append(chk)  # Add the Checkbutton to the list
+    lbl = tk.Label(status_frm, text=DEVICES_NAMES[i])
+    lbl.grid(column=1, row=i+2, sticky='nsew')  # Make the label fill the cell
+
+for i in range(3):
+    status_frm.grid_columnconfigure(i, weight=1)  # Allow the columns to expand when the window size changes
+
+# ---------------------------------Arrange Option Notebook----------------------------------
+option_notebook.add(stage_frm, text='打本')
+option_notebook.add(friendship_frm, text='羁绊')
+option_notebook.grid(column=0, row=1, sticky='nsew')  # Add the second Notebook to the grid
+map_choose_frm.grid(column=0, row=0, sticky="nsew")  # Make the frame fill the window
+map_label.grid(column=0, row=0, sticky="nsew")  # Add the Label to the grid
+map_choose_combobox.grid(column=1, row=0, padx=(0, 20), sticky="nsew")  # Add the Combobox to the grid
+run_times_label.grid(column=2, row=0, sticky="nsew")  # Add the Label to the grid with a horizontal padding of 10 pixels
+run_times_entry.grid(column=3, row=0, padx=(0, 20), sticky="nsew")  # Add the Entry to the grid with a horizontal padding of 10 pixels
+holder_label.grid(column=5, row=0, sticky="nsew")  # Add the Label to the grid
+holder_combobox.grid(column=6, row=0, sticky="nsew")  # Add the Combobox to the grid
+selectable_frm.grid(column=0, row=1, sticky="nsew")  # Make the frame fill the window
+selectable_frm_title.grid(column=0, row=0)  # Add the title to the grid
+resurrection_chk.grid(column=0, row=1, sticky='nsew')
+
+start_frm.grid(column=0, row=2, sticky="nsew")
+auto_run_button.grid(column=0, row=0, sticky='nsew', padx=150)
+
+root.mainloop()
--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,9 @@
 from airtest.core.api import *
+from globals import DEVICES
 import subprocess
 import sys
+import tkinter as tk
+from tkinter import ttk
 
 def set_device(func):
     """
@@ -43,3 +46,62 @@ def multiplayer_pass_stage(accountList: list):
         if exit_code != 0:
             print(f"Process {p.pid} exited with code {exit_code}, exiting...")
             sys.exit(exit_code)
+
+def add_to_accountlist(account:str, var: tk.BooleanVar, accountlist: list, holder_combobox: ttk.Combobox = None):
+    """
+    Add or remove an account from the account list based on the state of a checkbox.
+
+    Args:
+        account (str): The account associated with the Checkbutton.
+        var (tk.BooleanVar): The variable associated with the Checkbutton. Its value is True when the Checkbutton is checked, and False otherwise.
+        accountlist (list): The list of accounts. This function will add the account to this list when the Checkbutton is checked, and remove it when the Checkbutton is unchecked.
+        holder_combobox (ttk.Combobox): The Combobox that holds the account list.
+    """
+    holder_combobox.set("")
+    if var.get():
+        accountlist.append(account)
+    else:
+        accountlist.remove(account)
+    holder_combobox["values"] = accountlist
+    print(accountlist)  # Print the accountlist whenever a checkbox is checked or unchecked
+
+
+def check_connection(frm: ttk.Frame, checkbuttons: list = None, accountlist: list = None, vars: list = None, holder_combobox: ttk.Combobox = None)->list:
+    """
+    Check the connection status of each device and update the GUI.
+
+    Args:
+        frm (ttk.Frame): The frame in which the connection status indicators are displayed.
+        checkbuttons(list): check buttons for each device.
+        accountlist(list): The list of accounts to be onboard.
+        vars(list): The list of variables associated with the Checkbuttons.
+        holder_combobox (ttk.Combobox): The Combobox that holds the account list.
+    
+    Returns:
+        list: The connection status of the devices.
+    """
+    connection_status = []
+    accountlist.clear()
+    devices = list(DEVICES.keys())
+    device_urls = DEVICES.values()
+    holder_combobox.set("")
+    for device_url in device_urls:
+        try:
+            connect_device(device_url)
+            connection_status.append(True)  # Device is connected
+        except:
+            connection_status.append(False)  # Device is not connected
+    for i in range(len(DEVICES)):
+        color = 'green' if connection_status[i] else 'yellow'
+        cnv = tk.Canvas(frm, width=20, height=20)
+        cnv.grid(column=2, row=i+2)  # Make the canvas fill the cell
+        cnv.create_oval(5, 5, 15, 15, fill=color)
+        if checkbuttons is not None:
+            state = 'normal' if connection_status[i] else 'disabled'  # Set the state based on the connection status
+            checkbuttons[i].config(state=state)  # Update the state of the Checkbutton
+            vars[i].set(connection_status[i])
+        if connection_status[i]:
+            accountlist.append(devices[i])
+            print("Account list: ", accountlist)
+            holder_combobox["values"] = accountlist
+    return connection_status
